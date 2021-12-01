f76f66d122a302058ae51edacb52fa87cb7c0ca4
Guoming Yuan <guomingyuan@gmail.com>
2021-11-30 22:13:40 -0500

    Add About page

--- src/wxDemo/gen/frameMain.py
+++ src/wxDemo/gen/frameMain.py
@@ -17,7 +17,7 @@ import wx.xrc
 class frameMain ( wx.Frame ):
 
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"CCS simulator Demo", pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"CCS simulator Demo", pos = wx.DefaultPosition, size = wx.Size( 611,403 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
 		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
@@ -26,6 +26,22 @@ class frameMain ( wx.Frame ):
 		bSizerMainFrame = wx.BoxSizer( wx.VERTICAL )
 
 		self.m_panelMain = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		bSizer3 = wx.BoxSizer( wx.VERTICAL )
+
+		bSizer4 = wx.BoxSizer( wx.VERTICAL )
+
+		bSizer5 = wx.BoxSizer( wx.VERTICAL )
+
+
+		bSizer4.Add( bSizer5, 1, wx.EXPAND, 5 )
+
+
+		bSizer3.Add( bSizer4, 1, wx.EXPAND, 5 )
+
+
+		self.m_panelMain.SetSizer( bSizer3 )
+		self.m_panelMain.Layout()
+		bSizer3.Fit( self.m_panelMain )
 		bSizerMainFrame.Add( self.m_panelMain, 1, wx.EXPAND |wx.ALL, 0 )
 
 
@@ -59,6 +75,9 @@ class frameMain ( wx.Frame ):
 		self.menubarMain.Append( self.menuEdit, u"Edit" )
 
 		self.menuHelp = wx.Menu()
+		self.menuItemAbout = wx.MenuItem( self.menuHelp, wx.ID_ANY, u"About"+ u"\t" + u"F1", wx.EmptyString, wx.ITEM_NORMAL )
+		self.menuHelp.Append( self.menuItemAbout )
+
 		self.menubarMain.Append( self.menuHelp, u"Help" )
 
 		self.SetMenuBar( self.menubarMain )
@@ -72,6 +91,7 @@ class frameMain ( wx.Frame ):
 		self.Bind( wx.EVT_MENU, self.menuItemFileSaveOnMenuSelection, id = self.menuItemFileSave.GetId() )
 		self.Bind( wx.EVT_MENU, self.menuItemFileSaveAsOnMenuSelection, id = self.menuItemFileSaveAs.GetId() )
 		self.Bind( wx.EVT_MENU, self.menuItemFileExitOnMenuSelection, id = self.menuItemFileExit.GetId() )
+		self.Bind( wx.EVT_MENU, self.menuItemAboutOnMenuSelection, id = self.menuItemAbout.GetId() )
 
 	def __del__( self ):
 		pass
@@ -91,6 +111,54 @@ class frameMain ( wx.Frame ):
 		event.Skip()
 
 	def menuItemFileExitOnMenuSelection( self, event ):
+		event.Skip()
+
+	def menuItemAboutOnMenuSelection( self, event ):
+		event.Skip()
+
+
+###########################################################################
+## Class dialogAbout
+###########################################################################
+
+class dialogAbout ( wx.Dialog ):
+
+	def __init__( self, parent ):
+		wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = u"About", pos = wx.DefaultPosition, size = wx.Size( 340,250 ), style = wx.DEFAULT_DIALOG_STYLE )
+
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+		self.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_3DLIGHT ) )
+
+		bSizer7 = wx.BoxSizer( wx.VERTICAL )
+
+		self.m_staticText4 = wx.StaticText( self, wx.ID_ANY, u"\nyuehengDemo:\n\nLorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis ipsam maxime cumque inventore! Facere porro cumque quidem aliquam error itaque voluptatum, animi perferendis totam, inventore, doloribus optio reprehenderit necessitatibus quisquam.", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText4.Wrap( -1 )
+
+		bSizer7.Add( self.m_staticText4, 1, wx.EXPAND, 15 )
+
+		self.staticTextVersion = wx.StaticText( self, wx.ID_ANY, u"v 0.0", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.staticTextVersion.Wrap( -1 )
+
+		bSizer7.Add( self.staticTextVersion, 0, wx.ALIGN_CENTER|wx.ALL, 15 )
+
+		self.buttonOK = wx.Button( self, wx.ID_ANY, u"OK", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer7.Add( self.buttonOK, 0, wx.ALIGN_CENTER|wx.ALL, 5 )
+
+
+		self.SetSizer( bSizer7 )
+		self.Layout()
+
+		self.Centre( wx.BOTH )
+
+		# Connect Events
+		self.buttonOK.Bind( wx.EVT_BUTTON, self.buttonOKOnButtonClick )
+
+	def __del__( self ):
+		pass
+
+
+	# Virtual event handlers, override them in your derived class
+	def buttonOKOnButtonClick( self, event ):
 		event.Skip()
 
 
--- src/wxDemo/mainFrame.py
+++ src/wxDemo/mainFrame.py
@@ -1,4 +1,5 @@
-from .gen.frameMain import frameMain
+import wx
+from .gen.frameMain import frameMain, dialogAbout
 
 class MainFrame(frameMain):
     def __init__(self, parent):
@@ -21,7 +22,22 @@ class MainFrame(frameMain):
         event.Skip()
 
     def menuItemFileExitOnMenuSelection( self, event ):
-        print('You pressed Exit button')
+        self.Destroy()
         event.Skip()
 
+    def menuItemAboutOnMenuSelection( self, event ):
+        aboutPanel = AboutDialog(None)
+        aboutPanel.Show()
+        event.Skip()
 
+class AboutDialog(dialogAbout):
+    def __init__(self, parent):
+        dialogAbout.__init__(self, parent)
+        self.setVersion()
+
+    def buttonOKOnButtonClick( self, event ):
+        self.Destroy()
+        event.Skip()
+
+    def setVersion(self):
+        self.staticTextVersion.LabelText = '0.0.1.dev2'
